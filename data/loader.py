f20b6f0ad6ba2a3e7cf7223d131bb2316c991354
Saygafarov Ildar <sayildar17@gmail.com>
2023-08-13 22:59:28 +0500

    added proxy

--- data/loader.py
+++ data/loader.py
@@ -3,5 +3,5 @@ from aiogram import Bot, Dispatcher
 from . import config
 
 
-bot = Bot(token=config.BOT_TOKEN)
+bot = Bot(token=config.BOT_TOKEN, proxy="http://proxy.server:3128")
 dp = Dispatcher(bot)
